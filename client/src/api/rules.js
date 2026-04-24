2df926e8667643d8bc5580c11ecb8c57d6be8734
Peterngacha85 <peterngacha85@gmail.com>
2026-04-24 16:51:42 +0300

    fix: resolve import error in rules.js and clean up API paths

--- client/src/api/rules.js
+++ client/src/api/rules.js
@@ -1,6 +1,6 @@
-import API from './api';
+import api from './axios';
 
-export const getRules = () => API.get('/rules');
-export const createRule = (data) => API.post('/rules', data);
-export const updateRule = (id, data) => API.put(`/rules/${id}`, data);
-export const deleteRule = (id) => API.delete(`/rules/${id}`);
+export const getRules = () => api.get('rules');
+export const createRule = (data) => api.post('rules', data);
+export const updateRule = (id, data) => api.put(`rules/${id}`, data);
+export const deleteRule = (id) => api.delete(`rules/${id}`);
