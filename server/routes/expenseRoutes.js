e5945c597835ed38ac287a1c290c378547509f5d
Peterngacha85 <peterngacha85@gmail.com>
2026-05-15 12:58:40 +0300

    Feat: Add super admin CRUD to emergency kit and secure expenses

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const { getAllExpenses, createExpense, updateExpense, deleteExpense } = require('../controllers/expenseController');
 const { protect } = require('../middleware/authMiddleware');
 const { leaderOrSuperAdmin } = require('../middleware/roleMiddleware');
+const { requireApprovedSession } = require('../middleware/sessionMiddleware');
 
 router.get('/', protect, getAllExpenses);
-router.post('/', protect, leaderOrSuperAdmin, createExpense);
-router.put('/:id', protect, leaderOrSuperAdmin, updateExpense);
-router.delete('/:id', protect, leaderOrSuperAdmin, deleteExpense);
+router.post('/', protect, leaderOrSuperAdmin, requireApprovedSession, createExpense);
+router.put('/:id', protect, leaderOrSuperAdmin, requireApprovedSession, updateExpense);
+router.delete('/:id', protect, leaderOrSuperAdmin, requireApprovedSession, deleteExpense);
 
 module.exports = router;
